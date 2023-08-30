be28937114ed6079c2ca17ce5b82288a0ecb2acc
Елизавета Зуева <elizavetazueva@MacBook-Air-Elizaveta.local>
2023-08-30 18:10:33 +0300

    First commit

---
+++ app/src/main/java/com/example/neobis_android_chapter7/MainActivity.kt
@@ -0,0 +1,15 @@
+package com.example.neobis_android_chapter7
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.example.neobis_android_chapter7.databinding.ActivityMainBinding
+
+class MainActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityMainBinding
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+    }
+}
---
+++ app/src/main/java/com/example/neobis_android_chapter7/utils/Constants.java
@@ -0,0 +1,7 @@
+package com.example.neobis_android_chapter7.utils;
+
+class Constants {
+    companion object{
+        const val BASE_URL = "https://"
+    }
+}
